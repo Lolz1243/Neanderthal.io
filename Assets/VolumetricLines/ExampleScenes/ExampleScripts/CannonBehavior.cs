4b5cb41085b8c26a68c452da59eb3f647cd5005d
Nikhil Vytla <nvawesomeguy@gmail.com>
2019-01-13 16:32:09 -0500

    Added all assets and required files

---
+++ Assets/VolumetricLines/ExampleScenes/ExampleScripts/CannonBehavior.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBehavior : MonoBehaviour {
+
+	public Transform m_cannonRot;
+	public Transform m_muzzle;
+	public GameObject m_shotPrefab;
+	public Texture2D m_guiTexture;
+    public double shootPeriod;
+    private double count;
+    
+	// Use this for initialization
+	void Start () 
+	{
+        count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if (shootPeriod == count)
+		{
+			GameObject go = GameObject.Instantiate(m_shotPrefab, m_muzzle.position, m_muzzle.rotation) as GameObject;
+			GameObject.Destroy(go, 3f);
+            count = 0;
+		}
+        count++;
+	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(0f, 0f, m_guiTexture.width / 2, m_guiTexture.height / 2), m_guiTexture);
+	}
+}
